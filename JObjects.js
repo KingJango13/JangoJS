8737bb5e671bb44bb3a7f1af212c25c0458d0c7b
KingJango13 <97196637+KingJango13@users.noreply.github.com>
2023-10-31 14:53:07 -0400

    Update JObjects

--- JObjects.js
+++ JObjects.js
@@ -1,4 +1,71 @@
 (function(window) {
+    /**
+     * 
+     * @param {object} obj 
+     * @param {PropertyKey} property 
+     * @returns {boolean}
+     */
+    function hasOwn(obj, property) {
+        return Object.prototype.hasOwnProperty.call(obj, property);
+    }
+    function Tuple(array) {
+        if(!(this instanceof Tuple)) return new Tuple(array);
+        if(!Array.isArray(array)) throw "Tuple can only be constructed from an array";
+        for(let i = 0; i < array.length; i++) {
+            this[i] = array[i];
+        }
+        Object.defineProperty(this, "length", {
+            enumerable: false,
+            configurable: false,
+            writable: false,
+            value: array.length
+        });
+        return Object.preventExtensions(this);
+    }
+    Tuple.prototype = {};
+    Tuple.prototype[Symbol.iterator] = function*() {
+        for(let i = 0; i < this.length; i++) {
+            yield this[i];
+        }
+    }
+    let enums = {};
+    function Enum(obj) {
+        if(!(this instanceof Enum)) return new Enum(obj);
+        let members = [];
+        let byValue = {};
+        let hash = "DEADBEEF";
+        while(hash in enums) {
+            hash = Math.floor(Math.random() * 0x100000000).toString(16) + Math.floor(Math.random() * 0x100000000).toString(16);
+        }
+        Object.defineProperty(this, "__ENUM_HASH__", {
+            enumerable: false,
+            writable: false,
+            value: hash
+        });
+        for(let property in obj) {
+            if(!hasOwn(obj, property)) continue;
+            let val = obj[property];
+            if(!(typeof val === "string" || typeof val === "number")) continue;
+            members.push(property);
+            this[property] = val;
+            byValue[val] = property;
+        }
+        enums[hash] = {
+            data: Object.freeze(this),
+            members: members,
+            byValue
+        }
+        return enums[hash];
+    }
+    Enum.prototype = {};
+    Enum.prototype.members = function() {
+        return Array.from(enums[this.__ENUM_HASH__].members);
+    }
+    Enum.prototype.getNameForValue = function(value) {
+        if(!(typeof value === "string" || typeof value === "number"))
+            throw `Enums can only have values of type string or type number\nType received: ${typeof value}`;
+        return enums[this.__ENUM_HASH__].byValue[value];
+    }
     function createEnum(obj) {
         let enum_ = {};
         let byValueEnum = {};
@@ -32,7 +99,9 @@
         return Object.freeze(enum_);
     }
     const EXPORTS = {
-        createEnum
+        createEnum,
+        Tuple,
+        Enum
     };
     if(typeof module !== "undefined" && typeof module.exports !== "undefined") {
         module.exports = EXPORTS;
--- index.js
+++ index.js
@@ -3,5 +3,17 @@
         exports.string = require("./JStrings");
         exports.math = require("./JMath");
         exports.engineering = require("./Engineering");
+        exports.objects = require("./JObjects")
     }
+    window.jango = window.jango || {};
+    const SCRIPTS_PATH = "https://kingjango13.github.io/JangoJS";
+    async function importScript(name) {
+        return import(`https://kingjango13.github.io/JangoJS/${name}.js`);
+    }
+    (async function(){
+        await importScript("JStrings");
+        await importScript("JMath");
+        await importScript("Engineering");
+        await importScript("JObjects")
+    });
 })(this);
